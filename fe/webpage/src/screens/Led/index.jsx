5cbda61ccae55f871e2cb6a3e42ad40c87a66ef2
HaoPro <haopka.cnttvj@gmail.com>
2024-01-23 14:39:37 +0700

    new commit

--- fe/webpage/src/screens/Led/index.jsx
+++ fe/webpage/src/screens/Led/index.jsx
@@ -7,6 +7,8 @@ import { dataUrl, fetchData, sendData } from "../../AxiosUtility";
 const Led = () => {
   const [lightStatus, setLightStatus] = useState(false);
 
+  const ws = useRef(null);
+
   useEffect(() => {
     const fetchDataFromServer = async () => {
       try {
@@ -22,19 +24,35 @@ const Led = () => {
     };
 
     fetchDataFromServer();
+    // Connect to WebSocket server on ESP8266
+    ws.current = new WebSocket('ws://192.168.1.17:81');
+
+    // Set up WebSocket event listeners
+    ws.current.onopen = () => {
+      console.log('Connected to WebSocket');
+    };
+
+    ws.current.onmessage = (event) => {
+      console.log('Message from server:', event.data);
+    };
+
+    ws.current.onclose = () => {
+      console.log('Disconnected from WebSocket');
+    };
+
+    // Clean up WebSocket connection on component unmount
+    return () => {
+      ws.current.close();
+    };
   }, []);
 
-  const toggleLight = async () => {
-    try {
-      if (lightStatus) {
-        await sendData("http://192.168.1.17/LED=ON", "post", null);
-      } else {
-        await sendData("http://192.168.1.17/LED=OFF", "post", null);
-      }
-
+  const toggleLight = () => {
+    // Check if WebSocket connection is open before sending a message
+    if (ws.current.readyState === WebSocket.OPEN) {
+      // Send a "toggle" message to the WebSocket server
+      // Update the logic based on your requirements
+      ws.current.send('toggle');
       setLightStatus(!lightStatus);
-    } catch (error) {
-      console.error(error.message);
     }
   };
 
